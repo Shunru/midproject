f0da2040c8cd920f6149aa78a7de964acbeb59ce
Shun Xiao <shun.xiao@f1006-10-34127.bmcc.cuny.edu>
2015-11-02 16:30:15 -0500

    more change midtern

--- js/main.js
+++ js/main.js
@@ -3,5 +3,31 @@ function setup(){
 
 var cnv = createCanvas(windowWidth,windowHeight);
     cnv.parent('pattern');
+ var x = 40;
+ var y = 40;
+ var gridWidth = 1000;
+for(var i = 0; i<100; i++){
+
+//console.log(i);
+    drawPattern(x,y);
+//increment our x
+    x +=120;//same as x = x +=120
+    //check if x is past the grid width
+    if(x > gridWidth){
+    //set x back to the beginning
+        x=40;
+        //move y down
+        y+=300;
+    }//end for loop
+
+}//end for set up
+
+}
+
+function drawing(startX, startY){
+
+    triangle(0,30,40,0,40,30);
+
+
 
 }
